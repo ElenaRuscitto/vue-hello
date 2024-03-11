652ecc55bd09925398a0b3e99a2affc3dc175e16
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-03-11 15:07:22 +0100

    [add] 2 immage

--- js/script.js
+++ js/script.js
@@ -9,7 +9,8 @@ createApp ({
       message: 'Buon Pomeriggio',
       nome: 'Booleaners',
       // image: "./img/vuejs.jpg"
-      immagine2: '<img src="./img/vuejs.jpg" alt="">'
+      immagine2: '<img src="./img/vuejs.jpg" alt="">',
+      immagine3: 'https://c7.alamy.com/compit/2rht0h8/asp-concetto-di-business-e-teconologia-testo-su-cubi-di-legno-2rht0h8.jpg'
     }
   },
 
