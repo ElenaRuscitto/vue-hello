dc6687d2925188918233e8ceff9a0bfed002da9d
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-03-11 17:30:03 +0100

    [add] rifiniture

--- js/script.js
+++ js/script.js
@@ -8,13 +8,13 @@ createApp ({
 
       message: 'Buon Pomeriggio',
       nome: '',
-      myBGC: 'verdeScuro',
+
       myC: 'bianco',
       miaClasseMessage: 'verde',
       miaClasseNome: 'blu',
       displayDate:'00/00/0000',
       // image: "./img/vuejs.jpg"
-      immagine2: '<img src="./img/vuejs.jpg" alt="">',
+      immagine2: '<img src="./img/vuejs.jpg" alt="" class= "w-100">',
       immagine3: 'https://c7.alamy.com/compit/2rht0h8/asp-concetto-di-business-e-teconologia-testo-su-cubi-di-legno-2rht0h8.jpg'
     }
   },
@@ -23,9 +23,9 @@ createApp ({
     printDate(){
       const d =  new Date();
       const g = d.getDate() < 10 ? '0'+d.getDate() : d.getDate();
-      const m = d.getMonth() + 1 < 10 ? '0'+d.getMonth() : d.getMonth();
-      const a = d.getFullYear() ? '0'+d.getFullYear() : d.getFullYear();
-      this.displayData = `${g}/${m}/${a}`
+      const m = d.getMonth() + 1 < 10 ? '0'+(d.getMonth() + 1 ): d.getMonth() + 1;
+      const a = d.getFullYear() ;
+      this.displayDate = `${g}/${m}/${a}`
     }
   },
 
