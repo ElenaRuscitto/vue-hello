7f9fdcd429fb55b38f5ae59129533bc511bcffd7
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-03-11 15:37:45 +0100

    [add] css

--- js/script.js
+++ js/script.js
@@ -8,7 +8,9 @@ createApp ({
 
       message: 'Buon Pomeriggio',
       nome: '',
-
+      myBGC: 'verdeScuro',
+      miaClasseMessage: 'verde',
+      miaClasseNome: 'blu',
       // image: "./img/vuejs.jpg"
       immagine2: '<img src="./img/vuejs.jpg" alt="">',
       immagine3: 'https://c7.alamy.com/compit/2rht0h8/asp-concetto-di-business-e-teconologia-testo-su-cubi-di-legno-2rht0h8.jpg'
