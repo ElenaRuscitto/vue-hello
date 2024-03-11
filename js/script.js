7d7ea947c839b6018c899dd8baac3a21d1e759e5
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-03-11 16:24:01 +0100

    [add] data

--- js/script.js
+++ js/script.js
@@ -9,12 +9,28 @@ createApp ({
       message: 'Buon Pomeriggio',
       nome: '',
       myBGC: 'verdeScuro',
+      myC: 'bianco',
       miaClasseMessage: 'verde',
       miaClasseNome: 'blu',
+      displayDate:'00/00/0000',
       // image: "./img/vuejs.jpg"
       immagine2: '<img src="./img/vuejs.jpg" alt="">',
       immagine3: 'https://c7.alamy.com/compit/2rht0h8/asp-concetto-di-business-e-teconologia-testo-su-cubi-di-legno-2rht0h8.jpg'
     }
   },
+
+  methods: {
+    printDate(){
+      const d =  new Date();
+      const g = d.getDate() < 10 ? '0'+d.getDate() : d.getDate();
+      const m = d.getMonth() < 10 ? '0'+d.getMonth() : d.getMonth();
+      const a = d.getFullYear() < 10 ? '0'+d.getFullYear() : d.getFullYear();
+      this.displayClock = `${g}:${m}:${a}`
+    },
+  },
+
+  mounted(){
+    this.printData();
+  } ,
 
 }).mount ('#app');
